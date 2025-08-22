60ae02a408e94d77147c3ee3980379ac8dd37cd5
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2025-08-22 23:23:03 +0300

    add: transformers blocks

---
+++ model/transformers_block.py
@@ -0,0 +1,30 @@
+from torch import nn 
+from .attention import CausalSelfAttention
+
+class TransformerBlock(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_heads: int,
+        d_ff: int,
+        dropout: float = 0.0
+    ):
+        super().__init__()
+
+        self.ln1 = nn.LayerNorm(d_model)
+        self.attn = CausalSelfAttention(d_model, n_heads, dropout) 
+        self.ln2 = nn.LayerNorm(d_model)
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.GELU(),
+            nn.Linear(d_ff, d_model),
+            nn.Dropout(dropout)
+        )
+    
+    def forward(
+        self,
+        x: torch.Tensor
+    ):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
