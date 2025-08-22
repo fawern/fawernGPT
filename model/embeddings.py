4ef407c9492223e5be3b7a82008943c80f1d7fbe
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2025-08-22 23:25:43 +0300

    update: embeddings'

--- model/embeddings.py
+++ model/embeddings.py
@@ -4,9 +4,10 @@ from torch import nn
 class TokenPositionalEmbedding(nn.Module):
     def __init__(
         self, 
-        vocab_size
-        d_model=4096, 
-        dropout=0.0
+        vocab_size: int,
+        d_model: int = 4096, 
+        max_len: int = 1024,
+        dropout: float = 0.0
     ):
         super().__init__()
         self.token = nn.Embedding(vocab_size, d_model)
@@ -16,7 +17,7 @@ class TokenPositionalEmbedding(nn.Module):
     
     def forward(
         self, 
-        x
+        x: torch.Tensor 
     ): 
         b, t = x.shape 
         pos = torch.arange(t, device=x.device).unsqueeze(0).expand(b, t)
